aadf0a5e6016b9c6b01a888d1179002eb3f96e3f
Alikhan <ataubaldiyev@beeline.kz>
2021-05-16 16:27:34 +0600

    Настройка node heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ISCU</title>
-    <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script src="{{ asset('public/js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
